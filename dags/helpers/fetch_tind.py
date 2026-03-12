e226c54240161c81cf3f0d63f630c4a6b8c35295
yzhoubk <30507097+yzhoubk@users.noreply.github.com>
2026-03-12 12:10:06 -0700

    Ap 570: Implement TIND query fetcher task (#5)

---
+++ dags/helpers/fetch_tind.py
@@ -0,0 +1,70 @@
+import os
+import csv
+from typing import Union
+from pathlib import Path
+from lxml import etree
+from pymarc import Record, XMLWriter
+from typing import List
+from tind_client import TINDClient
+
+class FetchTind:
+    def __init__(self, run_id: str):
+        self.client = self._tind_client()
+        self.batch_dir = self._batch_dir(run_id)
+
+    def get_ids(self, tind_query: str) -> List[str]:
+        return  self.client.fetch_ids_search(tind_query)
+        
+    # to be used in later Jira ticket
+    def download_image_file(self, id: str) -> None:
+        record = self.client.fetch_file_metadata(id)     
+        download_url = record[0]["url"]    
+        record_dir = self._record_dir(id)        
+        self.client.fetch_file(download_url, record_dir)
+    
+    def download_metadata_file(self, id: str) -> None:
+        record = self.client.fetch_metadata(id)
+        file_path = Path(f"{self._record_dir(id)}/{id}.xml")
+        self._write_record_to_xml(record, file_path)
+
+    def save_tind_ids_file(self, ids: List[str]) -> None:
+        file_path = Path(f"{self.batch_dir}/ids.csv")
+        file_path.parent.mkdir(parents=True, exist_ok=True)
+
+        with file_path.open("w", newline="", encoding="utf-8") as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow(["TIND_ID"])
+            for id in ids:
+                writer.writerow([id])
+
+    def _tind_client(self) -> TINDClient:
+        base_dir = os.environ.get("MOKELUMNE_TIND_DOWNLOAD", "/opt/airflow/download")     
+        storage_dir = Path(base_dir)
+        storage_dir.mkdir(parents=True, exist_ok=True)
+        return TINDClient(default_storage_dir=storage_dir)
+     
+    def _record_dir(self, id: str) -> str:
+        record_dir = os.path.join(self.batch_dir, id)
+        os.makedirs(record_dir, exist_ok=True)
+        return record_dir
+
+    def _batch_dir(self, run_id: str) -> str:
+        return os.path.join(self.client.default_storage_dir, run_id)
+
+    def _write_record_to_xml(self, record: Record, file_path: Union[str, Path]) -> None:
+        file_path = Path(file_path)
+        file_path.parent.mkdir(parents=True, exist_ok=True)
+
+        with file_path.open("wb") as f:
+            writer = XMLWriter(f)
+            writer.write(record)
+            writer.close()
+
+        parser = etree.XMLParser(remove_blank_text=True)
+        tree = etree.parse(str(file_path), parser)
+        tree.write(
+            str(file_path),
+            encoding="utf-8",
+            xml_declaration=True,
+            pretty_print=True,
+        )
