7fa09515b66ea3a184663a38aee590c2f45c5385
A. Wilcox <AWilcox@Wilcox-Tech.com>
2026-03-20 20:19:42 -0500

    Refactor storage-related APIs into util module
    This allows us to use the same routines for all DAGs.  Also, install
    `mokelumne` in the environment so we can use util modules from DAGs.
    
    Ref: AP-615

--- dags/helpers/fetch_tind.py
+++ dags/helpers/fetch_tind.py
@@ -1,19 +1,20 @@
-import os
 import csv
 
 from pathlib import Path
-from typing import List, Union
+from typing import List
 
 from lxml import etree
 from pymarc import Record, XMLWriter
 from tind_client import TINDClient
 
+from mokelumne.util.storage import storage_dir, run_dir
+
 
 class FetchTind:
     """Helper methods for fetching items from TIND using TINDClient."""
     def __init__(self, run_id: str):
-        self.client = self._tind_client()
-        self.batch_dir = self._batch_dir(run_id)
+        self.client = TINDClient(default_storage_dir=str(storage_dir()))
+        self.batch_dir = run_dir(run_id)
 
     def get_ids(self, tind_query: str) -> List[str]:
         return  self.client.fetch_ids_search(tind_query)
@@ -23,16 +24,15 @@ class FetchTind:
         record = self.client.fetch_file_metadata(tind_id)
         download_url = record[0]["url"]
         record_dir = self._record_dir(tind_id)
-        self.client.fetch_file(download_url, record_dir)
+        self.client.fetch_file(download_url, str(record_dir))
 
     def download_metadata_file(self, tind_id: str) -> None:
         record = self.client.fetch_metadata(tind_id)
-        file_path = Path(f"{self._record_dir(tind_id)}/{tind_id}.xml")
+        file_path = self._record_dir(tind_id).joinpath(f"{tind_id}.xml")
         self._write_record_to_xml(record, file_path)
 
     def save_tind_ids_file(self, ids: List[str]) -> None:
-        file_path = Path(f"{self.batch_dir}/ids.csv")
-        file_path.parent.mkdir(parents=True, exist_ok=True)
+        file_path = self.batch_dir.joinpath("ids.csv")
 
         with file_path.open("w", newline="", encoding="utf-8") as csv_file:
             writer = csv.writer(csv_file)
@@ -40,24 +40,12 @@ class FetchTind:
             for tind_id in ids:
                 writer.writerow([tind_id])
 
-    def _tind_client(self) -> TINDClient:
-        base_dir = os.environ.get("MOKELUMNE_TIND_DOWNLOAD", "/opt/airflow/download")
-        storage_dir = Path(base_dir)
-        storage_dir.mkdir(parents=True, exist_ok=True)
-        return TINDClient(default_storage_dir=str(storage_dir))
-
-    def _record_dir(self, tind_id: str) -> str:
-        record_dir = os.path.join(self.batch_dir, tind_id)
-        os.makedirs(record_dir, exist_ok=True)
+    def _record_dir(self, tind_id: str) -> Path:
+        record_dir = self.batch_dir.joinpath(tind_id)
+        record_dir.mkdir(exist_ok=True)
         return record_dir
 
-    def _batch_dir(self, run_id: str) -> str:
-        return os.path.join(self.client.default_storage_dir, run_id)
-
-    def _write_record_to_xml(self, record: Record, file_path: Union[str, Path]) -> None:
-        file_path = Path(file_path)
-        file_path.parent.mkdir(parents=True, exist_ok=True)
-
+    def _write_record_to_xml(self, record: Record, file_path: Path) -> None:
         with file_path.open("wb") as f:
             writer = XMLWriter(f)
             writer.write(record)
---
+++ mokelumne/util/storage.py
@@ -0,0 +1,23 @@
+"""Provides storage management routines."""
+
+import os
+
+from pathlib import Path
+
+
+def storage_dir() -> Path:
+    """Retrieve the base directory path to use for storage."""
+    base_dir = os.environ.get("MOKELUMNE_TIND_DOWNLOAD", "/opt/airflow/download")
+    storage_path = Path(base_dir)
+    storage_path.mkdir(parents=True, exist_ok=True)
+    return storage_path
+
+
+def run_dir(run_id: str) -> Path:
+    """Retrieve the directory path to use for a given run's storage.
+
+    :note: As a side effect, calling this method will create the directory if it
+    does not already exist.  It is immediately usable for storage upon return."""
+    run_path = storage_dir().joinpath(run_id)
+    run_path.mkdir(parents=True, exist_ok=True)
+    return run_path
