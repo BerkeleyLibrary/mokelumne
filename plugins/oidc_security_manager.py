c9d42a390a3738cdf15dd51c02de5d1d20b4c7d4
Daniel Schmidt <danschmidt5189@berkeley.edu>
2026-03-31 23:22:35 -0700

    AP-631: Simplifies OIDC integrations
    - Consolidates Keycloak/Calnet OIDC into a single unified flow by configuring Keycloak to match Calnet's response structure.
    - Assigns unauthorized registered users the "Public" role. (This will change in an upcoming PR.)
    - Replaces manual well-known URL handling using FAB OAuth's server_metadata_url.
    - Adds a script to scaffold the local `.env` and updates the build accordingly, allowing GitHub secrets to be removed (fernet, jwt, etc.).
    - Updates the README with documentation for launching the local dev environment.
    - Simplifies, cleans up, and reorders the Compose File to make it easier to work with (find services, start them without waiting on deps, etc.).

--- plugins/oidc_security_manager.py
+++ plugins/oidc_security_manager.py
@@ -1,43 +1,24 @@
-import logging
 import os
-
-import jwt
-import requests
-
 from airflow.providers.fab.auth_manager.security_manager.override import FabAirflowSecurityManagerOverride
-from jwt import PyJWKClient
-
-log = logging.getLogger(__name__)
 
 class OIDCSecurityManager(FabAirflowSecurityManagerOverride):
-    """Provides a fab Security Manager implementation for Keycloak auth."""
-    def __init__(self, appbuilder):
-        super().__init__(appbuilder)
-        well_known = requests.get(os.getenv("OIDC_WELL_KNOWN")).json()
-        self.signing_algos = well_known["id_token_signing_alg_values_supported"]
-        self._jwks_client = PyJWKClient(well_known.get("jwks_uri"))
+    """
+    Provides a Security Manager implementation for CalNet/Keycloak.
+    """
 
-    # TODO depending on CAS reponse we may want to spin this out into an abstract class...
     def get_oauth_user_info(self, provider, response):
+        """
+        Extracts userinfo from Keycloak/Calnet OIDC response
+
+        FAB's upstream OAuth manager validates the JWT, so contrary
+        to the examples in Airflow's documentation we don't need
+        to do that here.
+        """
         if provider != os.getenv("OIDC_NAME"):
             return {}
 
-        token = response["access_token"]
-        try:
-            signing_key = self._jwks_client.get_signing_key_from_jwt(token)
-            me = jwt.decode(token, signing_key, algorithms=self.signing_algos)
-        except jwt.exceptions.PyJWTError as e:
-            log.exception("Failed to decode JWT")
-            return {}
-
-        # TODO needs CAS reponse to have groups in the top level
-        # currently is under attributes?
-        groups = [g.lstrip("/") for g in me.get("groups", [])]
-
-        userinfo = {
-            # TODO see what CAS returns and have this match?
-            "username": me.get("preferred_username"),
-            "role_keys": groups,
+        userinfo = response["userinfo"]
+        return {
+            "username": userinfo["preferred_username"],
+            "role_keys": userinfo["groups"],
         }
-
-        return userinfo
