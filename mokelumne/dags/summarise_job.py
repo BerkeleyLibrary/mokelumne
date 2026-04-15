BerkeleyLibrary/mokelumne

commit 490b66aebfc5af126b1b9def1add3a7553d33b4e

dags: Add summarise_job DAG
This collates the CSV files from the prior DAGs in the workflow and
places them all in a public directory with a given filename format.

It additionaly summarises the run and saves it to an HTML file, which
can be browsed on the Web or sent via email.  The publicly-accessible
output directory is emitted as an Asset.

Implements: AP-598

mokelumne/dags/summarise_job.py
@@ -1,0 +1,104 @@
+"""Collate the CSV output of the jobs and upload them to an object store."""
+
+from datetime import datetime, UTC
+from itertools import filterfalse
+from pathlib import Path
+from shutil import copyfile
+from uuid import uuid4
+
+import csv
+
+from airflow.sdk import dag, task, get_current_context, Asset
+
+from mokelumne.batch_image.assets import processed_csv
+from mokelumne.batch_image.assets import public_dir as public_dir_asset
+from mokelumne.util.storage import public_dir, public_path_to_url
+
+
+FILENAME_TEMPLATE: str = 'imagedesc_%s_{timestamp}.csv'
+"""The template for the generated filenames."""
+
+
+@dag(schedule=[processed_csv])
+def summarise_job():
+    """Summarise the DAG runs for the job."""
+
+    @task
+    def generate_id() -> str:
+        """Generate a URL-safe directory for collated output."""
+        path = public_dir() / str(uuid4())
+        path.mkdir()  # We don't exist_ok=True because it should be unique.
+        return str(path)
+
+    @task(inlets=[processed_csv])
+    def collate_csvs(out_str: str) -> str:
+        """Gather the CSVs from the DAG runs and place them in the given directory."""
+        context = get_current_context()
+        asset = context['triggering_asset_events'][processed_csv][0].asset
+        asset_path = Path(asset.uri.replace("file://", "")).parent
+        out_path = Path(out_str)
+
+        timestamp = datetime.now(UTC).strftime('%Y%m%d_%H%M%S')
+        template_name = FILENAME_TEMPLATE.format(timestamp=timestamp)
+
+        for csv in ('processed', 'fetched', 'skipped'):
+            copyfile(asset_path / f'{csv}.csv', out_path / (template_name % csv))
+
+        return timestamp
+
+    @task(outlets=[public_dir_asset])
+    def generate_summary(collated_str: str, timestamp: str) -> str:
+        """Generate a summary of the files in the collated path."""
+        collated_path = Path(collated_str)
+
+        template_name = FILENAME_TEMPLATE.format(timestamp=timestamp)
+        proc_path = collated_path / (template_name % 'processed')
+        fetched_path = collated_path / (template_name % 'fetched')
+        skipped_path = collated_path / (template_name % 'skipped')
+
+        with skipped_path.open(encoding='utf-8') as skipped:
+            skip_count = len(skipped.readlines()) - 1
+
+        with fetched_path.open(encoding='utf-8') as fetched:
+            reader = csv.reader(fetched)
+            header = next(reader)
+            all_fetched = list(reader)
+            fetch_count = len(all_fetched)
+            status_col = header.index('Status')
+            fetch_failures = len(list(filterfalse(lambda x: x == 'fetched',
+                                                  (row[status_col] for row in all_fetched))))
+            fetch_success = fetch_count - fetch_failures
+
+        with proc_path.open(encoding='utf-8') as processed:
+            reader = csv.reader(processed)
+            _header = next(reader)
+            proc_count = len(list(reader))
+
+        template_html = f"""
+        <html><head><title>Batch image description results</title></head><body>
+        <h1>Batch image description results for query</h1>
+        <dl>
+            <dt><a href="{public_path_to_url(proc_path)}">{proc_count} images processed</a></dt>
+            <dd>{proc_count} succeeded, {proc_count - fetch_success} failed</dd>
+            <dt><a href="{public_path_to_url(fetched_path)}">{fetch_count} images fetched</a></dt>
+            <dd>{fetch_success} succeeded, {fetch_failures} failed</dd>
+            <dt><a href="{public_path_to_url(skipped_path)}">{skip_count} records skipped</a></dt>
+            <dd>Records did not match filter criteria</dd>
+        </dl>
+        </body></html>
+        """
+
+        with (collated_path / 'index.html').open('w') as html:
+            html.write(template_html)
+
+        context = get_current_context()
+        context["outlet_events"][public_dir_asset].add(Asset(f"file://{collated_str}"))
+
+        return collated_str
+
+    directory = generate_id()
+    csv_timestamp = collate_csvs(directory)
+    generate_summary(directory, csv_timestamp)
+
+
+summarise_job()

.dockerignore
@@ -1,3 +1,6 @@
 # Ignore generated files
 **/*.py[cod]
 **/__pycache__
+
+download/
+logs/

.gitignore
@@ -5,6 +5,7 @@ tmp
 artifacts/*
 logs/*
 download/*
+public/*
 __pycache__/
 *.py[cod]
 *$py.class

docker-compose.yml
@@ -68,6 +68,7 @@ x-airflow-common: &airflow-common
     - ./:/opt/airflow
     - ./artifacts:/home/airflow/artifacts
     - ./test:/home/airflow/test
+    - ./public:/opt/airflow/public
   user: "${AIRFLOW_UID:-40093}:0"
 
 services:

mokelumne/batch_image/assets.py
@@ -44,3 +44,10 @@ encountered one or more failures during processing.
 Emitted by: `generate_image_descriptions`
 Consumed by: `notify_user`
 """
+
+public_dir = AssetAlias('mokelumne/batch-image/public-dir')
+"""The publicly accessible directory where user-facing assets are stored.
+
+Emitted by: `summarise_job`
+Consumed by: `notify_user`
+"""

mokelumne/util/storage.py
@@ -28,3 +28,20 @@ def record_dir(run_id: str, record_id: str) -> Path:
     record_path = run_dir(run_id) / record_id[0:2] / record_id
     record_path.mkdir(exist_ok=True, parents=True)
     return record_path
+
+
+def public_dir() -> Path:
+    """Retrieve the base directory path to use for *public* storage."""
+    public_env = os.environ.get("MOKELUMNE_PUBLIC_STORAGE", "/opt/airflow/public")
+    public_path = Path(public_env)
+    public_path.mkdir(exist_ok=True)
+    return public_path
+
+
+def public_path_to_url(public_path: Path) -> str:
+    """Take a public path, as a subdirectory of +public_dir()+, and return a publicly accessible
+    HTTPS URL."""
+    top = public_dir()
+    location = public_path.relative_to(top)
+    url_base = os.environ.get("MOKELUMNE_PUBLIC_URL", "https://mokelumne-assets.ucblib.org/")
+    return f"{url_base}{str(location)}"

commit cdc6e5918bf069b3d96a96c501c51765089176d9

dags: Tag `summarise_job` properly
This was missed in the initial creation of this DAG.

Fixes: 490b66aebf ("dags: Add summarise_job DAG")
Ref: AP-598

mokelumne/dags/summarise_job.py
@@ -19,7 +19,7 @@ FILENAME_TEMPLATE: str = 'imagedesc_%s_{timestamp}.csv'
 """The template for the generated filenames."""
 
 
-@dag(schedule=[processed_csv])
+@dag(schedule=[processed_csv], catchup=False, tags=["batch-image", "csv", "summary"])
 def summarise_job():
     """Summarise the DAG runs for the job."""
 