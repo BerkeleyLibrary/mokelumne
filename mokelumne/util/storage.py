4dd2d17b9c7e4be743ea9de1c0339ee4992e6f82
maría a. matienzo <73732+anarchivist@users.noreply.github.com>
2026-05-27 12:16:54 -0500

    refactor storage methods to allow passing an explicit base_dir (#74)
    the LDC fetcher introduced in AP-664 needs to be able to write to a different
    volume, but we want to ensure our code stays DRY. this modifies the
    mokelumne.storage storage_dir() and run_dir() methods to allow a base_dir
    argument if we want to write job output to a different location across
    dags.

--- mokelumne/util/storage.py
+++ mokelumne/util/storage.py
@@ -3,22 +3,23 @@
 import os
 
 from pathlib import Path
+from typing import Optional
 
-
-def storage_dir() -> Path:
+def storage_dir(base_dir: Optional[str] = None) -> Path:
     """Retrieve the base directory path to use for storage."""
-    base_dir = os.environ.get("MOKELUMNE_TIND_DOWNLOAD", "/opt/airflow/download")
+    if not base_dir:
+        base_dir = os.environ.get("MOKELUMNE_TIND_DOWNLOAD", "/opt/airflow/download")
     storage_path = Path(base_dir)
     storage_path.mkdir(parents=True, exist_ok=True)
     return storage_path
 
 
-def run_dir(run_id: str) -> Path:
+def run_dir(run_id: str, base_dir: Optional[str] = None) -> Path:
     """Retrieve the directory path to use for a given run's storage.
 
     :note: As a side effect, calling this method will create the directory if it
     does not already exist.  It is immediately usable for storage upon return."""
-    run_path = storage_dir() / run_id
+    run_path = storage_dir(base_dir=base_dir) / run_id
     run_path.mkdir(exist_ok=True)
     return run_path
 
