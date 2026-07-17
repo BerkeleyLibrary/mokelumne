afa2a6bcf5c1ebd1b15eee4e6939dfe39d462983
maría a. matienzo <73732+anarchivist@users.noreply.github.com>
2026-07-17 08:01:37 -0700

    AP-789: upgrade to Airflow 3.3.0 (#87)
    * AP-789: upgrade to Airflow 3.3.0
    
    also moves provider-specific dependencies out of pyproject.toml, etc.
    
    * add updated mokelumne provider packages that work with airflow 3.3.0
    
    * DagBag no longer has an include_examples arg in Airflow 3.3.0

--- test/tests/test_dags.py
+++ test/tests/test_dags.py
@@ -10,7 +10,7 @@ dag_dir = Path(__file__).resolve().parent.parent.parent / "mokelumne/dags"
 
 @pytest.fixture()
 def dagbag() -> DagBag:
-    return DagBag(dag_folder=dag_dir.resolve(), include_examples=False)
+    return DagBag(dag_folder=dag_dir.resolve())
 
 
 def test_dags_load_with_no_errors(dagbag: DagBag) -> None:
